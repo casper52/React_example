e37295d9da171557b1585fafcc6f0877cb53b76b
casper52 <casper_52@naver.com>
2019-01-23 11:42:18 +0900

    react example

---
+++ src/comp1/Counter.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+
+class Counter extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            count: 0,
+            amount: props.amount
+        }
+    }
+
+    clickHandle = () =>{
+        console.log("clicked..." + this.props.amount)
+        this.setState({
+            count: this.state.count + this.state.amount
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h3>{ this.state.count }</h3>
+                <button onClick={ this.clickHandle }>CLICK</button>
+                <hr></hr>
+            </div>
+        )
+    }
+}
+
+export default Counter
